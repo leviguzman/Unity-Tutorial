9397017d2d5da6e7e7c1eedb364aa1822c5313a1
leviguzman <jeremy.levi.guzman@gmail.com>
2022-08-09 16:39:06 -0500

    Add files via upload

--- AnimationAndMovementController.cs
+++ AnimationAndMovementController.cs
@@ -5,14 +5,23 @@ using UnityEngine.InputSystem;
 
 public class AnimationAndMovementController : MonoBehaviour
 {
-    //declare ref variables
+    //declare reference variables
     PlayerInput playerInput;
     CharacterController characterController;
     Animator animator;
 
+    // variables to store optimized set/get paramerter IDs
+    int isWalkingHash;
+    int isRunningHash; 
+
+    //variables for player input value storage
     Vector2 currentMovementInput;
     Vector3 currentMovement;
+    Vector3 currentRunMovement;
     bool isMovementPressed;
+    bool isRunPressed;
+    float rotationFactorPerFrame = 10f;
+    float runMultiplier = 3.0f;
 
     void Awake()
     {
@@ -21,39 +30,107 @@ public class AnimationAndMovementController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
 
+        isWalkingHash = Animator.StringToHash ("IsWalking");
+        isRunningHash = Animator.StringToHash("IsRunning");
+
         playerInput.CharacterControls.Move.started += onMovementInput;
         playerInput.CharacterControls.Move.canceled += onMovementInput;
         playerInput.CharacterControls.Move.performed += onMovementInput;
+        playerInput.CharacterControls.Run.started += onRun;
+        playerInput.CharacterControls.Run.canceled += onRun;
+    }
+
+    void onRun(InputAction.CallbackContext context)
+    {
+        isRunPressed = context.ReadValueAsButton();
+    }
+
+    void handleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
+
     }
 
     void onMovementInput (InputAction.CallbackContext context)
     {
-            currentMovementInput = context.ReadValue<Vector2>();
-            currentMovement.x = currentMovementInput.x;
-            currentMovement.z = currentMovementInput.y;
-            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;     
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        currentRunMovement.x = currentMovementInput.x * runMultiplier;
+        currentRunMovement.z = currentMovementInput.y * runMultiplier;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;     
     }
 
     void handleAnimation()
     {
-        bool isWalking = animator.GetBool("IsWalking");
-        bool isRunning = animator.GetBool("IsRunning");
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
 
         if (isMovementPressed && !isWalking)
         {
-            animator.SetBool("IsWalking", true);
+            animator.SetBool(isWalkingHash, true);
         }
 
         else if (!isMovementPressed && isWalking)
         {
-            animator.SetBool("IsWalking", false);
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        if ((isMovementPressed && isRunPressed) && !isRunning)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        else if ((!isMovementPressed || !isRunPressed) && isRunning)
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+
+
+    }
+
+    void handleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            float groundedGravity = -.05f;
+            currentMovement.y = groundedGravity;
+            currentRunMovement.y = groundedGravity;
+        }
+        else
+        {
+            float gravity = -9.8f;
+            currentMovement.y += gravity;
+            currentRunMovement.y += gravity;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        handleGravity();
+        handleRotation();
         handleAnimation();
+
+        if (isRunPressed)
+        {
+            characterController.Move(currentRunMovement * Time.deltaTime);
+        } else
+            {
+            characterController.Move(currentMovement * Time.deltaTime);
+            }
         characterController.Move(currentMovement * Time.deltaTime);
     }
 
